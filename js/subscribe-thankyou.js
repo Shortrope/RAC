b4a842a767e7ae6dc12eb9fe31a3a53ddd31353a
Mario Kaack <mario@shortrope.com>
2015-08-09 09:46:53 -0700

    Initial commit
    In the middle of altering the info-boxes of
    the home page.
    Most pages are built.

---
+++ js/subscribe-thankyou.js
@@ -0,0 +1,6 @@
+$('#subscribe-submit').click(function(event) {
+    event.preventDefault();
+    $('#subscribe-form').hide();
+    $('#subscribe-thankyou').show();
+//    $('#subscribe-thankyou').css("display","block");
+});
